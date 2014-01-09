d9aae01d171853480530c3dbda8c5188efb31187
Dave Remba <daveremba@yahoo.com>
2014-01-08 17:30:24 -0800

    pch off due to compile errors with min/max redef

--- samples/BasicApp/vc2013_winrt/basicAppXAML/App.xaml.cpp
+++ samples/BasicApp/vc2013_winrt/basicAppXAML/App.xaml.cpp
@@ -3,7 +3,23 @@
 // Implementation of the App class.
 //
 
-#include "pch.h"
+// zv pch off
+// #include "pch.h"
+#include <wrl.h>
+#include <wrl/client.h>
+#include <d3d11_2.h>
+#include <d2d1_2.h>
+#include <d2d1effects_1.h>
+#include <dwrite_2.h>
+#include <wincodec.h>
+#include <DirectXColors.h>
+#include <DirectXMath.h>
+#include <memory>
+#include <agile.h>
+#include <concrt.h>
+#include <collection.h>
+#include "App.xaml.h"
+
 #include "CinderPage.xaml.h"
 
 using namespace basicAppXAML;
--- samples/BasicApp/vc2013_winrt/basicAppXAML/Common/DeviceResources.cpp
+++ samples/BasicApp/vc2013_winrt/basicAppXAML/Common/DeviceResources.cpp
@@ -1,4 +1,20 @@
-﻿#include "pch.h"
+﻿// zv pch off
+// #include "pch.h"
+#include <wrl.h>
+#include <wrl/client.h>
+#include <d3d11_2.h>
+#include <d2d1_2.h>
+#include <d2d1effects_1.h>
+#include <dwrite_2.h>
+#include <wincodec.h>
+#include <DirectXColors.h>
+#include <DirectXMath.h>
+#include <memory>
+#include <agile.h>
+#include <concrt.h>
+#include <collection.h>
+#include "App.xaml.h"
+
 #include "DeviceResources.h"
 #include "DirectXHelper.h"
 #include <windows.ui.xaml.media.dxinterop.h>
