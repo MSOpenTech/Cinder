b2467babdb092880f1044b1bce7f702512b133b0
Dale Stammen <dalestam@microsoft.com>
2014-01-29 09:53:10 -0800

    moved xaml support files into cinder lib

---
+++ include/cinder/app/winrt/xaml/Common/StepTimer.h
@@ -0,0 +1,217 @@
+﻿//
+// class DX::StepTimer
+// a timer for DirectX and related interfaces.
+// This is a recent framework that is up to date for Windows 8.1, WinRT, and XAML.
+//
+
+// The copyright in this software is being made available under the BSD License, included below. 
+// This software may be subject to other third party and contributor rights, including patent rights, 
+// and no such rights are granted under this license.
+//
+// Copyright (c) 2013, Microsoft Open Technologies, Inc. 
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, 
+// are permitted provided that the following conditions are met:
+//
+// - Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer.
+// - Redistributions in binary form must reproduce the above copyright notice, 
+//   this list of conditions and the following disclaimer in the documentation 
+//   and/or other materials provided with the distribution.
+// - Neither the name of Microsoft Open Technologies, Inc. nor the names of its contributors 
+//   may be used to endorse or promote products derived from this software 
+//   without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+// EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#pragma once
+
+#include <wrl.h>
+
+namespace DX
+{
+	// Helper class for animation and simulation timing.
+	class StepTimer
+	{
+	public:
+		StepTimer() : 
+			m_elapsedTicks(0),
+			m_totalTicks(0),
+			m_leftOverTicks(0),
+			m_frameCount(0),
+			m_framesPerSecond(0),
+			m_framesThisSecond(0),
+			m_qpcSecondCounter(0),
+			m_isFixedTimeStep(false),
+			m_targetElapsedTicks(TicksPerSecond / 60)
+		{
+			if (!QueryPerformanceFrequency(&m_qpcFrequency))
+			{
+				throw ref new Platform::FailureException();
+			}
+
+			if (!QueryPerformanceCounter(&m_qpcLastTime))
+			{
+				throw ref new Platform::FailureException();
+			}
+
+			// Initialize max delta to 1/10 of a second.
+			m_qpcMaxDelta = m_qpcFrequency.QuadPart / 10;
+		}
+
+		// Get elapsed time since the previous Update call.
+		uint64 GetElapsedTicks() const						{ return m_elapsedTicks; }
+		double GetElapsedSeconds() const					{ return TicksToSeconds(m_elapsedTicks); }
+
+		// Get total time since the start of the program.
+		uint64 GetTotalTicks() const						{ return m_totalTicks; }
+		double GetTotalSeconds() const						{ return TicksToSeconds(m_totalTicks); }
+
+		// Get total number of updates since start of the program.
+		uint32 GetFrameCount() const						{ return m_frameCount; }
+
+		// Get the current framerate.
+		uint32 GetFramesPerSecond() const					{ return m_framesPerSecond; }
+
+		// Set whether to use fixed or variable timestep mode.
+		void SetFixedTimeStep(bool isFixedTimestep)			{ m_isFixedTimeStep = isFixedTimestep; }
+
+		// Set how often to call Update when in fixed timestep mode.
+		void SetTargetElapsedTicks(uint64 targetElapsed)	{ m_targetElapsedTicks = targetElapsed; }
+		void SetTargetElapsedSeconds(double targetElapsed)	{ m_targetElapsedTicks = SecondsToTicks(targetElapsed); }
+
+		// Integer format represents time using 10,000,000 ticks per second.
+		static const uint64 TicksPerSecond = 10000000;
+
+		static double TicksToSeconds(uint64 ticks)			{ return static_cast<double>(ticks) / TicksPerSecond; }
+		static uint64 SecondsToTicks(double seconds)		{ return static_cast<uint64>(seconds * TicksPerSecond); }
+
+		// After an intentional timing discontinuity (for instance a blocking IO operation)
+		// call this to avoid having the fixed timestep logic attempt a set of catch-up 
+		// Update calls.
+
+		void ResetElapsedTime()
+		{
+			if (!QueryPerformanceCounter(&m_qpcLastTime))
+			{
+				throw ref new Platform::FailureException();
+			}
+
+			m_leftOverTicks = 0;
+			m_framesPerSecond = 0;
+			m_framesThisSecond = 0;
+			m_qpcSecondCounter = 0;
+		}
+
+		// Update timer state, calling the specified Update function the appropriate number of times.
+		template<typename TUpdate>
+		void Tick(const TUpdate& update)
+		{
+			// Query the current time.
+			LARGE_INTEGER currentTime;
+
+			if (!QueryPerformanceCounter(&currentTime))
+			{
+				throw ref new Platform::FailureException();
+			}
+
+			uint64 timeDelta = currentTime.QuadPart - m_qpcLastTime.QuadPart;
+
+			m_qpcLastTime = currentTime;
+			m_qpcSecondCounter += timeDelta;
+
+			// Clamp excessively large time deltas (e.g. after paused in the debugger).
+			if (timeDelta > m_qpcMaxDelta)
+			{
+				timeDelta = m_qpcMaxDelta;
+			}
+
+			// Convert QPC units into a canonical tick format. This cannot overflow due to the previous clamp.
+			timeDelta *= TicksPerSecond;
+			timeDelta /= m_qpcFrequency.QuadPart;
+
+			uint32 lastFrameCount = m_frameCount;
+
+			if (m_isFixedTimeStep)
+			{
+				// Fixed timestep update logic
+
+				// If the app is running very close to the target elapsed time (within 1/4 of a millisecond) just clamp
+				// the clock to exactly match the target value. This prevents tiny and irrelevant errors
+				// from accumulating over time. Without this clamping, a game that requested a 60 fps
+				// fixed update, running with vsync enabled on a 59.94 NTSC display, would eventually
+				// accumulate enough tiny errors that it would drop a frame. It is better to just round 
+				// small deviations down to zero to leave things running smoothly.
+
+				if (abs(static_cast<int64>(timeDelta - m_targetElapsedTicks)) < TicksPerSecond / 4000)
+				{
+					timeDelta = m_targetElapsedTicks;
+				}
+
+				m_leftOverTicks += timeDelta;
+
+				while (m_leftOverTicks >= m_targetElapsedTicks)
+				{
+					m_elapsedTicks = m_targetElapsedTicks;
+					m_totalTicks += m_targetElapsedTicks;
+					m_leftOverTicks -= m_targetElapsedTicks;
+					m_frameCount++;
+
+					update();
+				}
+			}
+			else
+			{
+				// Variable timestep update logic.
+				m_elapsedTicks = timeDelta;
+				m_totalTicks += timeDelta;
+				m_leftOverTicks = 0;
+				m_frameCount++;
+
+				update();
+			}
+
+			// Track the current framerate.
+			if (m_frameCount != lastFrameCount)
+			{
+				m_framesThisSecond++;
+			}
+
+			if (m_qpcSecondCounter >= static_cast<uint64>(m_qpcFrequency.QuadPart))
+			{
+				m_framesPerSecond = m_framesThisSecond;
+				m_framesThisSecond = 0;
+				m_qpcSecondCounter %= m_qpcFrequency.QuadPart;
+			}
+		}
+
+	private:
+		// Source timing data uses QPC units.
+		LARGE_INTEGER m_qpcFrequency;
+		LARGE_INTEGER m_qpcLastTime;
+		uint64 m_qpcMaxDelta;
+
+		// Derived timing data uses a canonical tick format.
+		uint64 m_elapsedTicks;
+		uint64 m_totalTicks;
+		uint64 m_leftOverTicks;
+
+		// Members for tracking the framerate.
+		uint32 m_frameCount;
+		uint32 m_framesPerSecond;
+		uint32 m_framesThisSecond;
+		uint64 m_qpcSecondCounter;
+
+		// Members for configuring fixed timestep mode.
+		bool m_isFixedTimeStep;
+		uint64 m_targetElapsedTicks;
+	};
+}
