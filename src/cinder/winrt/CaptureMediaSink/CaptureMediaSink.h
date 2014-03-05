fe812dec7468d47fe2259087308266b665355f8f
Dave Remba <daveremba@yahoo.com>
2014-03-04 18:08:00 -0800

    added WRL::RuntimeClass decl's for CSink

--- src/cinder/winrt/CaptureMediaSink/CaptureMediaSink.h
+++ src/cinder/winrt/CaptureMediaSink/CaptureMediaSink.h
@@ -1,1 +1,36 @@
 ﻿#pragma once
+
+#include "pch.h"
+
+// seems needed to get the definition of RuntimeClass_CaptureMediaSink_CaptureSink below
+// but wasn't in template for some reason
+#include "CaptureMediaSink_h.h"
+
+class CSink
+    : public Microsoft::WRL::RuntimeClass<
+    Microsoft::WRL::RuntimeClassFlags< Microsoft::WRL::RuntimeClassType::WinRtClassicComMix >,
+    ABI::Windows::Media::IMediaExtension >
+//    IMFTransform >
+{
+    InspectableClass(RuntimeClass_CaptureMediaSink_CaptureSink, BaseTrust)
+
+public:
+    CSink() {}
+
+    ~CSink() {}
+
+    STDMETHOD(RuntimeClassInitialize)()
+    {
+        return S_OK;
+    }
+
+    // declare methods needed for each interface used by the CSink class:
+
+    // IMediaExtension
+    STDMETHODIMP SetProperties(ABI::Windows::Foundation::Collections::IPropertySet *pConfiguration)
+    {
+        return S_OK;
+    }
+
+};
+
