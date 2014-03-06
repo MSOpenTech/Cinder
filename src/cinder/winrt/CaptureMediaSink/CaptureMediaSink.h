f1b2dca459a43a4188a9249d0397ffd30871bdc3
Dave Remba <daveremba@yahoo.com>
2014-03-06 11:46:58 -0800

    added IMFTransform to interface as stubs returning S_OK

--- src/cinder/winrt/CaptureMediaSink/CaptureMediaSink.h
+++ src/cinder/winrt/CaptureMediaSink/CaptureMediaSink.h
@@ -9,8 +9,8 @@
 class CSink
     : public Microsoft::WRL::RuntimeClass<
     Microsoft::WRL::RuntimeClassFlags< Microsoft::WRL::RuntimeClassType::WinRtClassicComMix >,
-    ABI::Windows::Media::IMediaExtension >
-//    IMFTransform >
+    ABI::Windows::Media::IMediaExtension,
+    IMFTransform >
 {
     InspectableClass(RuntimeClass_CaptureMediaSink_CaptureSink, BaseTrust)
 
@@ -31,6 +31,195 @@ public:
     {
         return S_OK;
     }
+
+    // IMFTransform
+    STDMETHODIMP GetStreamLimits(
+        DWORD   *pdwInputMinimum,
+        DWORD   *pdwInputMaximum,
+        DWORD   *pdwOutputMinimum,
+        DWORD   *pdwOutputMaximum
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP GetStreamCount(
+        DWORD   *pcInputStreams,
+        DWORD   *pcOutputStreams
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP GetStreamIDs(
+        DWORD   dwInputIDArraySize,
+        DWORD   *pdwInputIDs,
+        DWORD   dwOutputIDArraySize,
+        DWORD   *pdwOutputIDs
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP GetInputStreamInfo(
+        DWORD                     dwInputStreamID,
+        MFT_INPUT_STREAM_INFO *   pStreamInfo
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP GetOutputStreamInfo(
+        DWORD                     dwOutputStreamID,
+        MFT_OUTPUT_STREAM_INFO *  pStreamInfo
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP GetAttributes(IMFAttributes** pAttributes)
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP GetInputStreamAttributes(
+        DWORD           dwInputStreamID,
+        IMFAttributes   **ppAttributes
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP GetOutputStreamAttributes(
+        DWORD           dwOutputStreamID,
+        IMFAttributes   **ppAttributes
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP DeleteInputStream(DWORD dwStreamID)
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP AddInputStreams(
+        DWORD   cStreams,
+        DWORD   *adwStreamIDs
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP GetInputAvailableType(
+        DWORD           dwInputStreamID,
+        DWORD           dwTypeIndex, // 0-based
+        IMFMediaType    **ppType
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP GetOutputAvailableType(
+        DWORD           dwOutputStreamID,
+        DWORD           dwTypeIndex, // 0-based
+        IMFMediaType    **ppType
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP SetInputType(
+        DWORD           dwInputStreamID,
+        IMFMediaType    *pType,
+        DWORD           dwFlags
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP SetOutputType(
+        DWORD           dwOutputStreamID,
+        IMFMediaType    *pType,
+        DWORD           dwFlags
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP GetInputCurrentType(
+        DWORD           dwInputStreamID,
+        IMFMediaType    **ppType
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP GetOutputCurrentType(
+        DWORD           dwOutputStreamID,
+        IMFMediaType    **ppType
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP GetInputStatus(
+        DWORD           dwInputStreamID,
+        DWORD           *pdwFlags
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP GetOutputStatus(DWORD *pdwFlags)
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP SetOutputBounds(
+        LONGLONG        hnsLowerBound,
+        LONGLONG        hnsUpperBound
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP ProcessEvent(
+        DWORD              dwInputStreamID,
+        IMFMediaEvent      *pEvent
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP ProcessMessage(
+        MFT_MESSAGE_TYPE    eMessage,
+        ULONG_PTR           ulParam
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP ProcessInput(
+        DWORD               dwInputStreamID,
+        IMFSample           *pSample,
+        DWORD               dwFlags
+        )
+    {
+        return S_OK;
+    }
+
+    STDMETHODIMP ProcessOutput(
+        DWORD                   dwFlags,
+        DWORD                   cOutputBufferCount,
+        MFT_OUTPUT_DATA_BUFFER  *pOutputSamples, // one per stream
+        DWORD                   *pdwStatus
+        )
+    {
+        return S_OK;
+    }
+
+
 
 };
 
