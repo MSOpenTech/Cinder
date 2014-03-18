a579ca3f98d3e0d3a2486db4afba0fde135df041
Dave Remba <daveremba@yahoo.com>
2014-03-17 20:12:27 -0700

    removed ABI namespace for non-WinRT COM class

--- src/cinder/winrt/CaptureMediaSink/CaptureMediaStreamSink.h
+++ src/cinder/winrt/CaptureMediaSink/CaptureMediaStreamSink.h
@@ -35,92 +35,91 @@ namespace Microsoft {
 }
 #endif
 
-namespace ABI {
-    namespace CaptureMediaSink {
+namespace CaptureMediaSink {
 
-        public delegate void SampleHandler( /* BufferCore::IMediaBufferReference^ sample */);
+    public delegate void SampleHandler( /* BufferCore::IMediaBufferReference^ sample */);
 
-        // WrlSealed
-        class CaptureMediaStreamSink :
-            public Microsoft::WRL::RuntimeClass<
-            Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::ClassicCom>
-            /*
-            IMFStreamSink
-            IMFMediaEventGenerator,
-            IMFMediaTypeHandler
-            */
-            >
-        {
-        public:
+    // WrlSealed
+    class CaptureMediaStreamSink :
+        public Microsoft::WRL::RuntimeClass<
+        Microsoft::WRL::RuntimeClassFlags<Microsoft::WRL::ClassicCom>
+        /*
+        IMFStreamSink
+        IMFMediaEventGenerator,
+        IMFMediaTypeHandler
+        */
+        >
+    {
+    public:
 
-            CaptureMediaStreamSink() {}
+        CaptureMediaStreamSink() {}
 
-            HRESULT RuntimeClassInitialize(__in IMFMediaSink* sink, __in DWORD id, __in IMFMediaType* mt, CaptureMediaSink::SampleHandler^ sampleHandler);
+        HRESULT RuntimeClassInitialize(__in IMFMediaSink* sink, __in DWORD id, __in IMFMediaType* mt, CaptureMediaSink::SampleHandler^ sampleHandler);
 
 #if 0
 
-            HRESULT InternalSetCurrentMediaType(__in IMFMediaType *mediaType);
+        HRESULT InternalSetCurrentMediaType(__in IMFMediaType *mediaType);
 
-            //
-            // IMFStreamSink
-            //
+        //
+        // IMFStreamSink
+        //
 
-            IFACEMETHODIMP GetMediaSink(__deref_out IMFMediaSink **sink);
-            IFACEMETHODIMP GetIdentifier(__out DWORD *identifier);
-            IFACEMETHODIMP GetMediaTypeHandler(__deref_out IMFMediaTypeHandler **handler);
-            IFACEMETHODIMP ProcessSample(__in_opt IMFSample *sample);
-            IFACEMETHODIMP PlaceMarker(__in MFSTREAMSINK_MARKER_TYPE markerType, __in const PROPVARIANT * markerValue, __in const PROPVARIANT * contextValue);
-            IFACEMETHODIMP Flush();
+        IFACEMETHODIMP GetMediaSink(__deref_out IMFMediaSink **sink);
+        IFACEMETHODIMP GetIdentifier(__out DWORD *identifier);
+        IFACEMETHODIMP GetMediaTypeHandler(__deref_out IMFMediaTypeHandler **handler);
+        IFACEMETHODIMP ProcessSample(__in_opt IMFSample *sample);
+        IFACEMETHODIMP PlaceMarker(__in MFSTREAMSINK_MARKER_TYPE markerType, __in const PROPVARIANT * markerValue, __in const PROPVARIANT * contextValue);
+        IFACEMETHODIMP Flush();
 
-            //
-            // IMFMediaEventGenerator
-            //
+        //
+        // IMFMediaEventGenerator
+        //
 
-            IFACEMETHODIMP GetEvent(__in DWORD flags, __deref_out IMFMediaEvent **event);
-            IFACEMETHODIMP BeginGetEvent(__in IMFAsyncCallback *callback, __in_opt IUnknown *state);
-            IFACEMETHODIMP EndGetEvent(__in IMFAsyncResult *result, __deref_out IMFMediaEvent **event);
-            IFACEMETHODIMP QueueEvent(__in MediaEventType met, __in REFGUID extendedType, __in HRESULT status, __in_opt const PROPVARIANT *value);
+        IFACEMETHODIMP GetEvent(__in DWORD flags, __deref_out IMFMediaEvent **event);
+        IFACEMETHODIMP BeginGetEvent(__in IMFAsyncCallback *callback, __in_opt IUnknown *state);
+        IFACEMETHODIMP EndGetEvent(__in IMFAsyncResult *result, __deref_out IMFMediaEvent **event);
+        IFACEMETHODIMP QueueEvent(__in MediaEventType met, __in REFGUID extendedType, __in HRESULT status, __in_opt const PROPVARIANT *value);
 
-            //
-            // IMFMediaTypeHandler
-            //
+        //
+        // IMFMediaTypeHandler
+        //
 
-            IFACEMETHODIMP IsMediaTypeSupported(__in IMFMediaType *mediaType, __deref_out_opt  IMFMediaType **closestMediaType);
-            IFACEMETHODIMP GetMediaTypeCount(__out DWORD *typeCount);
-            IFACEMETHODIMP GetMediaTypeByIndex(__in DWORD index, __deref_out  IMFMediaType **mediaType);
-            IFACEMETHODIMP SetCurrentMediaType(__in IMFMediaType *mediaType);
-            IFACEMETHODIMP GetCurrentMediaType(__deref_out_opt IMFMediaType **mediaType);
-            IFACEMETHODIMP GetMajorType(__out GUID *majorType);
+        IFACEMETHODIMP IsMediaTypeSupported(__in IMFMediaType *mediaType, __deref_out_opt  IMFMediaType **closestMediaType);
+        IFACEMETHODIMP GetMediaTypeCount(__out DWORD *typeCount);
+        IFACEMETHODIMP GetMediaTypeByIndex(__in DWORD index, __deref_out  IMFMediaType **mediaType);
+        IFACEMETHODIMP SetCurrentMediaType(__in IMFMediaType *mediaType);
+        IFACEMETHODIMP GetCurrentMediaType(__deref_out_opt IMFMediaType **mediaType);
+        IFACEMETHODIMP GetMajorType(__out GUID *majorType);
 
-            //
-            // Misc
-            //
+        //
+        // Misc
+        //
 
-            HRESULT RequestSample();
-            void Shutdown();
+        HRESULT RequestSample();
+        void Shutdown();
 #endif
-        private:
+    private:
 
-            HRESULT _VerifyMediaType(__in IMFMediaType* mt) const;
-            HRESULT _UpdateMediaType(__in IMFMediaType* mt);
+        HRESULT _VerifyMediaType(__in IMFMediaType* mt) const;
+        HRESULT _UpdateMediaType(__in IMFMediaType* mt);
 
-            Microsoft::WRL::ComPtr<IMFMediaSink> _sink;
-            // Microsoft::WRL::ComPtr<IMFMediaEventQueue> _eventQueue;
-            Microsoft::WRL::ComPtr<IMFMediaType> _curMT;
+        Microsoft::WRL::ComPtr<IMFMediaSink> _sink;
+        // Microsoft::WRL::ComPtr<IMFMediaEventQueue> _eventQueue;
+        Microsoft::WRL::ComPtr<IMFMediaType> _curMT;
 
 
-            /// Microsoft::WRL::ComPtr<MediaCore::InteropServices::IMediaBufferNativeFactory> _factory;
+        /// Microsoft::WRL::ComPtr<MediaCore::InteropServices::IMediaBufferNativeFactory> _factory;
 
-            /// MediaReaders::SampleHandler^ _sampleHandler;
+        /// MediaReaders::SampleHandler^ _sampleHandler;
 
-            GUID _majorType;
-            GUID _subType;
-            unsigned int _width;
-            unsigned int _height;
-            DWORD _id;
-            bool _shutdown;
+        GUID _majorType;
+        GUID _subType;
+        unsigned int _width;
+        unsigned int _height;
+        DWORD _id;
+        bool _shutdown;
 
-            Microsoft::WRL::Wrappers::SRWLock _lock;
-        };
-    }
+        Microsoft::WRL::Wrappers::SRWLock _lock;
+    };
 }
+
