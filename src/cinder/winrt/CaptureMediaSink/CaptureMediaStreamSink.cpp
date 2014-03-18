934ab408f3bab0ab81aef7454f54174ac6acd84f
Dave Remba <daveremba@yahoo.com>
2014-03-17 20:13:00 -0700

    new cpp file for CaptureMediaStreamSink class

---
+++ src/cinder/winrt/CaptureMediaSink/CaptureMediaStreamSink.cpp
@@ -0,0 +1,23 @@
+﻿//////////////////////////////////////////////////////////////////////////
+//
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+//////////////////////////////////////////////////////////////////////////
+
+// CaptureMediaSink.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "pch.h"
+#include "CaptureMediaStreamSink_h.h"
+#include "CaptureMediaStreamSink.h"
+
+namespace CaptureMediaSink {
+
+    ActivatableClass(CaptureMediaStreamSink);
+
+}
