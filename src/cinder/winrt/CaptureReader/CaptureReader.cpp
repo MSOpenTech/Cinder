a5d7d1d9bb5ff9f344018d1de18078411787a1ad
Dave Remba <daveremba@yahoo.com>
2014-04-07 19:11:09 -0700

    added static lib CaptureReader project to try to use SourceReader approach - does not compile for WinRT

---
+++ src/cinder/winrt/CaptureReader/CaptureReader.cpp
@@ -0,0 +1,92 @@
+﻿//////////////////////////////////////////////////////////////////////////
+//
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "pch.h"
+#include "CaptureReader.h"
+
+
+// version using MFSourceReader
+// since we are not using a DLL for the media sink, add the Media Foundation Libraries
+// directly into the App build
+#pragma comment(lib, "mfplat")
+#pragma comment(lib, "mf")
+#pragma comment(lib, "mfuuid")
+
+
+// see Enumerating Video Capture Devices
+// http://msdn.microsoft.com/en-us/library/windows/desktop/dd940326(v=vs.85).aspx
+//
+HRESULT CreateVideoDeviceSource(IMFMediaSource **ppSource)
+{
+    *ppSource = NULL;
+
+    IMFMediaSource *pSource = NULL;
+    IMFAttributes *pAttributes = NULL;
+    IMFActivate **ppDevices = NULL;
+
+    // Create an attribute store to specify the enumeration parameters.
+    HRESULT hr = MFCreateAttributes(&pAttributes, 1);
+    if (FAILED(hr))
+    {
+        goto done;
+    }
+
+    // Source type: video capture devices
+    hr = pAttributes->SetGUID(
+        MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE,
+        MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID
+        );
+    if (FAILED(hr))
+    {
+        goto done;
+    }
+
+    // Enumerate devices.
+    UINT32 count;
+    hr = MFEnumDeviceSources(pAttributes, &ppDevices, &count);
+    if (FAILED(hr))
+    {
+        goto done;
+    }
+
+    if (count == 0)
+    {
+        hr = E_FAIL;
+        goto done;
+    }
+
+    // Create the media source object.
+    hr = ppDevices[0]->ActivateObject(IID_PPV_ARGS(&pSource));
+    if (FAILED(hr))
+    {
+        goto done;
+    }
+
+    *ppSource = pSource;
+    (*ppSource)->AddRef();
+
+done:
+    SafeRelease(&pAttributes);
+
+    for (DWORD i = 0; i < count; i++)
+    {
+        SafeRelease(&ppDevices[i]);
+    }
+    CoTaskMemFree(ppDevices);
+    SafeRelease(&pSource);
+    return hr;
+}
+
+
+void start()
+{
+
+}
---
+++ src/cinder/winrt/CaptureReader/pch.h
@@ -0,0 +1,21 @@
+﻿//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#endif
+
+// Windows Header Files:
+#include <windows.h>
+
+// Media Foundation
+#include <mfapi.h>
+#include <mfidl.h>
+#include <mfobjects.h>
+#include <mfreadwrite.h>
