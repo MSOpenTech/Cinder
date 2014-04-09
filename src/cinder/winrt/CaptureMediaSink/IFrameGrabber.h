e689e41bc160a3ba09efdadeaf22df689a17c537
Dave Remba <daveremba@yahoo.com>
2014-04-09 13:03:06 -0700

    new IFrameGrabber interface

---
+++ src/cinder/winrt/CaptureMediaSink/IFrameGrabber.h
@@ -0,0 +1,54 @@
+//////////////////////////////////////////////////////////////////////////
+//
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+//////////////////////////////////////////////////////////////////////////
+
+// Interface to a FrameGrabber
+
+#pragma once
+
+
+#include <rpc.h>
+#include <rpcndr.h>
+
+typedef interface IFrameGrabber IFrameGrabber;
+
+MIDL_INTERFACE("715C10A9-D1E6-4B64-9961-A414CD470E89")
+IFrameGrabber : public IInspectable
+{
+public:
+    virtual HRESULT STDMETHODCALLTYPE GetFrameCount(
+        DWORD *pCount ) = 0;
+};
+
+extern const __declspec(selectany) IID & IID_IFrameGrabber = __uuidof(IFrameGrabber);
+
+#if 0
+namespace FrameGrabber {
+
+    class CFrameGrabber
+        : public Microsoft::WRL::RuntimeClass <
+        Microsoft::WRL::RuntimeClassFlags< Microsoft::WRL::RuntimeClassType::WinRtClassicComMix >,
+        IFrameGrabber
+        >
+    {
+        InspectableClass(L"FrameGrabber.CFrameGrabber", BaseTrust)
+    public:
+        CFrameGrabber() {}
+
+        ~CFrameGrabber() {}
+
+        HRESULT RuntimeClassInitialize()
+        {
+            HRESULT hr = S_OK;
+        }
+    };
+
+}
+#endif
