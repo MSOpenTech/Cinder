d5505ab7f5ac2d0dca2f328c2bf74fe4bdd2ab6b
Dave Remba <daveremba@yahoo.com>
2014-04-15 10:43:07 -0700

    reformat only

--- samples/CaptureBasic/src/CaptureBasicAppWinRT.cpp
+++ samples/CaptureBasic/src/CaptureBasicAppWinRT.cpp
@@ -22,19 +22,19 @@ using namespace std;
 // was:
 // class CaptureBasicApp : public AppNative {
 class CaptureBasicApp : public AppBasic {
-  public:
+public:
     void prepareSettings(Settings *settings);
 
     void setup();
     static void setupCompletion();
 
-    void keyDown( KeyEvent event );
-	
-	void update();
-	void draw();
+    void keyDown(KeyEvent event);
 
-	CaptureRef			mCapture;
-	dx::TextureRef		mTexture;
+    void update();
+    void draw();
+
+    CaptureRef			mCapture;
+    dx::TextureRef		mTexture;
 };
 
 void CaptureBasicApp::prepareSettings(Settings *settings)
@@ -48,36 +48,36 @@ void CaptureBasicApp::setup()
 {
 #if 1
 
-    Capture::getDevicesAsync( true, [=]() {
+    Capture::getDevicesAsync(true, [=]() {
         for (auto device = Capture::getDevices().begin(); device != Capture::getDevices().end(); ++device) {
             std::string s = (*device)->getName();
             console() << "Device: " << (*device)->getName() << " "
 #if defined( CINDER_COCOA_TOUCH )
-					<< ( (*device)->isFrontFacing() ? "Front" : "Rear" ) << "-facing"
+                << ((*device)->isFrontFacing() ? "Front" : "Rear") << "-facing"
 #endif
-					<< std::endl;
+                << std::endl;
         }
     });
 
-	try {
-        mCapture = Capture::create( 640, 480 );
-		mCapture->start();
-	}
-	catch( ... ) {
-		console() << "Failed to initialize capture" << std::endl;
-	}
+    try {
+        mCapture = Capture::create(640, 480 );
+        mCapture->start();
+    }
+    catch (...) {
+        console() << "Failed to initialize capture" << std::endl;
+    }
 #endif
 }
 
-void CaptureBasicApp::keyDown( KeyEvent event )
+void CaptureBasicApp::keyDown(KeyEvent event)
 {
 #if defined( CINDER_MAC )
-	if( event.getChar() == 'f' )
-		setFullScreen( ! isFullScreen() );
-	else if( event.getChar() == ' ' )
-		( mCapture && mCapture->isCapturing() ) ? mCapture->stop() : mCapture->start();
+    if( event.getChar() == 'f' )
+        setFullScreen( ! isFullScreen() );
+    else if( event.getChar() == ' ' )
+        (mCapture && mCapture->isCapturing()) ? mCapture->stop() : mCapture->start();
 #endif
-//    if (event.getChar() == 'T')
+    //    if (event.getChar() == 'T')
     if (event.getChar() == ' ') {
         if (mCapture && mCapture->isCapturing()) mCapture->stop();
     }
@@ -86,9 +86,9 @@ void CaptureBasicApp::keyDown( KeyEvent event )
 void CaptureBasicApp::update()
 {
 #if 1
-	if( mCapture && mCapture->checkNewFrame() ) {
-		mTexture = dx::Texture::create( mCapture->getSurface() );
-	}
+    if (mCapture && mCapture->checkNewFrame()) {
+        mTexture = dx::Texture::create(mCapture->getSurface());
+    }
 #endif
 }
 
@@ -96,24 +96,24 @@ void CaptureBasicApp::draw()
 {
     dx::clear(Color(0.1f, 0.5f, 0.15f));
 
-//	dx::clear( Color( 0.0f, 0.0f, 0.0f ) );
+    //	dx::clear( Color( 0.0f, 0.0f, 0.0f ) );
 
-    dx::setMatricesWindow( getWindowWidth(), getWindowHeight() );
-	
-	if( mTexture ) {
-		// glPushMatrix();
+    dx::setMatricesWindow(getWindowWidth(), getWindowHeight());
+
+    if (mTexture) {
+        // glPushMatrix();
         dx::pushMatrices();
 #if defined( CINDER_COCOA_TOUCH )
-		//change iphone to landscape orientation
-		dx::rotate( 90.0f );
-		dx::translate( 0.0f, -getWindowWidth() );
+        //change iphone to landscape orientation
+        dx::rotate( 90.0f );
+        dx::translate( 0.0f, -getWindowWidth() );
 
-		Rectf flippedBounds( 0.0f, 0.0f, getWindowHeight(), getWindowWidth() );
-		dx::draw( mTexture, flippedBounds );
+        Rectf flippedBounds( 0.0f, 0.0f, getWindowHeight(), getWindowWidth() );
+        dx::draw(mTexture, flippedBounds);
 #else
-		dx::draw( mTexture );
+        dx::draw(mTexture);
 #endif
-		// glPopMatrix();
+        // glPopMatrix();
         dx::popMatrices();
     }
 }
